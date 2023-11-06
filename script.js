41d91044870464c3a1243b733203cc6c61353667
citronoiseau <avroraka2004@gmail.com>
2023-11-06 16:54:57 +0200

    added general score and full restart

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let userWin = false;
 let computerWin = false;
 let buttonChoice = document.querySelectorAll(".weapon");
 
-let restartGame = document.querySelector(".restart");
+let computerOverall = 0;
+let userOverall = 0;
+
+let startNew = document.querySelector(".newGame");
+let restartAll = document.querySelector(".restartGame");
 
 const choiceContainer = document.querySelector(`.choiceContainer`);
 const userChoiceContainer = document.createElement(`div`);
@@ -43,6 +47,16 @@ win.textContent = ``;
 getScore.appendChild(score);
 getScore.appendChild(win);
 
+const getOverallScore = document.querySelector(`.overallScore`);
+const overallScore = document.createElement(`div`);
+const scoreInfo = document.createElement(`p`);
+overallScore.classList.add("overallScoreText");
+scoreInfo.classList.add("overallScoreText");
+overallScore.textContent = ``;
+scoreInfo.textContent = ``;
+getOverallScore.appendChild(scoreInfo);
+getOverallScore.appendChild(overallScore);
+
 function game() {
   buttonChoice.forEach(function (button) {
     button.addEventListener(`click`, getChoices);
@@ -60,7 +74,8 @@ function getChoices(event) {
     computerChoice.textContent = `Computer's choice: `;
 
     playRound(computerSelection, userSelection);
-    restartGame.style.display = "block";
+    startNew.style.display = "block";
+    restartAll.style.display = "block";
 
     if (userWin) {
       computerLives--;
@@ -69,14 +84,20 @@ function getChoices(event) {
     }
     score.textContent = `User Lives: ${userLives}, Computer Lives: ${computerLives}`;
 
-    if (userLives === 0) {
-      win.textContent = `Computer wins the game!`;
-    } else if (computerLives === 0) {
-      win.textContent = `User wins the game!`;
-    }
+    updateScoreTable();
   }
 }
-
+function updateScoreTable() {
+  if (userLives === 0) {
+    win.textContent = `Computer wins the game!`;
+    computerOverall++;
+  } else if (computerLives === 0) {
+    win.textContent = `User wins the game!`;
+    userOverall++;
+  }
+  scoreInfo.textContent = `Overall Score:`;
+  overallScore.textContent = `User Score: ${userOverall} Computer Score: ${computerOverall}`;
+}
 function getComputerChoice() {
   let choiceIndex = Math.floor(Math.random() * choice.length);
   let computerChoice = choice[choiceIndex];
@@ -101,7 +122,7 @@ function playRound(computerSelection, userSelection) {
   }
 }
 
-function restart() {
+function newGame() {
   win.textContent = ``;
   computerChoice.textContent = ``;
   userChoice.textContent = ``;
@@ -111,9 +132,28 @@ function restart() {
   computerSelectionPhoto.src = ``;
   userLives = 5;
   computerLives = 5;
-  restartGame.style.display = "none";
+  startNew.style.display = "none";
   game();
 }
-restartGame.addEventListener(`click`, restart);
+function restartGame() {
+  win.textContent = ``;
+  computerChoice.textContent = ``;
+  userChoice.textContent = ``;
+  score.textContent = ``;
+  result.textContent = ``;
+  userSelectionPhoto.src = ``;
+  computerSelectionPhoto.src = ``;
+  userLives = 5;
+  computerLives = 5;
+  userOverall = 0;
+  computerOverral = 0;
+  startNew.style.display = "none";
+  restartAll.style.display = "none";
+  overallScore.textContent = ``;
+  scoreInfo.textContent = ``;
+  game();
+}
+startNew.addEventListener(`click`, newGame);
+restartAll.addEventListener(`click`, restartGame);
 
 game();
